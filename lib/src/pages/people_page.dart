93795154e2bd4f7fbe29c4215c5f98ebc84069a9
Roddy Vitali <roddicks@gmail.com>
2020-04-12 20:49:07 -0400

    view people

---
+++ lib/src/pages/people_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PeoplePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        Text('People 1'),
+        Text('People 2'),
+        Text('People 3'),
+        Text('People 4'),
+        Text('People 5'),
+        Text('People 6'),
+      ],
+    );
+  }
+}
